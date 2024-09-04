e395462910a3296b09e72c40fe10984bc7051c75
KI JONG BYUN <kjbyun0@gmail.com>
2024-09-04 11:37:16 -0500

    Add my solution

--- javascript/tree_traversal_inorder.js
+++ javascript/tree_traversal_inorder.js
@@ -6,8 +6,57 @@ class Node {
   }
 }
 
+// Recursive solution
+// function treeTraversalInorder(root) {
+//   // type your code here
+//   if (root === null)
+//     return [];
+
+//   console.log("curr: ", root.value, 
+//     ", left: ", root.left === null ? null : root.left.value,
+//     ", right: ", root.right === null ? null : root.right.value);
+  
+//   const res = treeTraversalInorder(root.left);
+//   res.push(root.value);
+//   res.push(...treeTraversalInorder(root.right));
+//   return res;
+// }
+
+// Iterative solution
 function treeTraversalInorder(root) {
-  // type your code here
+  if (root === null)
+    return [];
+
+  class StackItem {
+    constructor(node=null, visited=false) {
+      this.node = node;
+      this.visited = visited;
+    }
+  }
+
+  const stack = [];
+  const res = [];
+  stack.push(new StackItem(root, false));
+  while (stack.length > 0) {
+    const sItem = stack.pop();
+    const curNode = sItem.node;
+    if (sItem.visited) {
+      res.push(curNode.value);
+    } else {
+      if (curNode.right !== null) {
+        const sItemRight = new StackItem(curNode.right, false);
+        stack.push(sItemRight);
+      }
+      sItem.visited = true;
+      stack.push(sItem);
+      if (curNode.left !== null) {
+        const sItemLeft = new StackItem(curNode.left, false);
+        stack.push(sItemLeft);
+      }
+    }
+  }
+
+  return res;
 }
 
 if (require.main === module) {
@@ -20,6 +69,28 @@ if (require.main === module) {
 
   root = new Node(10, new Node(0, null, new Node(5)), new Node(20, null, new Node(30)));
   console.log("Expecting: [0, 5, 10, 20, 30] ");
+  console.log(treeTraversalInorder(root));
+
+  root = new Node(1);
+  console.log("");
+  console.log("Expecting: [1]");
+  console.log(treeTraversalInorder(root));
+
+  root.left = new Node(2);
+  console.log("");
+  console.log("Expecting: [2, 1]");
+  console.log(treeTraversalInorder(root));
+
+  root.right = new Node(3);
+  console.log("");
+  console.log("Expecting: [2, 1. 3]");
+  console.log(treeTraversalInorder(root));
+
+  root = new Node(1);
+  root.left = new Node(2);
+  root.left.right = new Node(3);
+  console.log("");
+  console.log("Expecting: [2, 3, 1]");
   console.log(treeTraversalInorder(root));
 }
 
